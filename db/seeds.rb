ab5b41667374defadcc4823746aa2d957ce5ae98
darrengtan <darrengtan@gmail.com>
2015-08-22 20:23:08 -0700

    figured out seed dump

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,62 +1,73 @@
-tourn1_results = "[[[[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1]],[[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1]],[[2,1],[2,1],[2,1],[2,1]],[[2,1],[2,1]],[[2,1],[1,2]]]]"
+User.create!([
+  {email: "guest@aa.io", password_digest: "$2a$10$EO4r9eC8nTXRnRlwGBRgXOD0PHj9rtEFqX7bOFOZcdfUvzJlJ/ata", session_token: "NcQh-shQN7RurB_-4FXY_Q", username: "Guest"},
+  {email: "lmb@aa.io", password_digest: "$2a$10$Cjtca8/cCt0hxltKO/ZOAOiuiilVFxR3oo0GkbgYB0oCIWCUOKoxS", session_token: "ObTYuEwHm3WVX3r1vNWJtA", username: "LiMuBai"},
+  {email: "ysl@aa.io", password_digest: "$2a$10$1vKhDY792H0IqluE970yFeypGtufILNFvI78SXZP7odkW.9cHiaHW", session_token: "QbW0tLK6HiFVYTS0Wl3OYQ", username: "YuShuLien"},
+  {email: "plf@aa.io", password_digest: "$2a$10$Es6Sfs7Env.hVv7kF8.yOuOC4ETGj8D11v00cPXPoLxGajeOyWvWO", session_token: "A9bdmFIijq87ega6hNJEAA", username: "PeteLaFleur"},
+  {email: "wg@aa.io", password_digest: "$2a$10$3nZ6OVXkH21zIOUADnIWvukQSU0pUbx0iyr.EYjmGnBUsgA96lmfm", session_token: "E41LEUXgcK8OyymU3EXWpQ", username: "WhiteGoodman"},
+  {email: "patches@aa.io", password_digest: "$2a$10$edLni.UOZr5A7IQ7Nh39bOdgNuuEMEdv0fQ7ZduCz0ZafmxhTNNVy", session_token: "Aezm_x_YGTudRCjw9KreEg", username: "PatchesFanatic"},
+  {email: "mml@aa.io", password_digest: "$2a$10$OUa9LfhJB.u2GsCRCS.Ni.sgqcM3dmS/rH6zfdDzREbGn0wPRMKOG", session_token: "WmVENd76dD9wyHZwDzFJqQ", username: "ManMythLegend"},
+  {email: "chucknorris@aa.io", password_digest: "$2a$10$FMVeL0Sl.TFuRBXBdlt.jOor5j/eIuRMdQaPJSBmEA6.El/ttGkL6", session_token: "s5Eru33Pi2hYMIC7f6itow", username: "ChuckNorris"}
+])
 
-user1 = User.create(email: "guest@aa.io", username: "Guest", password: "tourneygen")
-user2 = User.create(email: "lmb@aa.io", username: "LiMuBai", password: "hiddentigers")
-user3 = User.create(email: "ysl@aa.io", username: "YuShuLien", password: "crouchingdragons")
-user4 = User.create(email: "plf@aa.io", username: "PeteLaFleur", password: "waytooaverage")
-user5 = User.create(email: "wg@aa.io", username: "WhiteGoodman", password: "globogym")
-user6 = User.create(email: "patches@aa.io", username: "PatchesFanatic", password: "allhailwrenches")
-user7 = User.create(email: "mml@aa.io", username: "ManMythLegend", password: "themanthemyththelegend")
-user8 = User.create(email: "chucknorris@aa.io", username: "ChuckNorris", password: "chucknorrisfacts")
+Team.create!([
+  {owner_id: 2, name: "Crouching Tigers", description: "No growth without assistance. No action without reaction. No desire without restraint. Now give yourself up and find yourself again."},
+  {owner_id: 3, name: "Hidden Dragons", description: "Fighters have rules, too. Friendship, trust, integrity. Always keep your promise. Without rules we wouldn't survive long."},
+  {owner_id: 4, name: "Average Joe's", description: "Average Joe's is a local team named under the gym they are trying to save from bankruptcy. Led by its owner Pete LaFleur and coached by dodgeball legend 'Patches' O'Houlihan, they hope to win all dodgeball tournaments to keep their gym afloat."},
+  {owner_id: 5, name: "Purple Cobras", description: "You ready for the, whoooo, hurricane?"},
+  {owner_id: 6, name: "The Patches", description: "Devastated by the loss of Patches in the movie Dodgeball, a group of fanatics have teamed up to pay their respects to Patches with one last tournament."},
+  {owner_id: 7, name: "Legends", description: "The Man, The Myth, The Legend has graced the tournament floor. Do you dare stand in his way to glory?"},
+  {owner_id: 8, name: "Chuck Norris", description: "Fear of spiders is called arachnophobia, fear of tight spaces is called claustrophobia, fear of Chuck Norris is called logic."}
+])
 
-tourn1 = Tournament.create(title: "1st Tournament Tutorial", author_id: user1.id, description: "Welcome to the first tournament ever hosted on TournaGen! This is an example of a tournament starting from scratch. Take a look around!", max_teams: 32, results: tourn1_results)
-tourn2 = Tournament.create(title: "If You can Dodge a Wrench", author_id: user2.id, description: "Welcome to the official dogeball tournament for TournaGen!", max_teams: 8)
-tourn3 = Tournament.create(title: "May Madness", author_id: user1.id, description: "It's the 31st annual May Madness tournament! Live out your predictions for March Madness here. Disclaimer: None of these teams are actually the NCAA teams they're portraying.", max_teams: 8)
+Image.create!([
+  {url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/v1440296455/dlm1nl0gjnktjtto3mmn.jpg", thumb_url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/c_scale,h_150,w_150/v1440296455/dlm1nl0gjnktjtto3mmn.jpg", imageable_id: 1, imageable_type: "Team"},
+  {url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/v1440296658/z1jhnctvhaayzz4hij7w.jpg", thumb_url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/c_scale,h_150,w_150/v1440296658/z1jhnctvhaayzz4hij7w.jpg", imageable_id: 2, imageable_type: "Team"},
+  {url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/v1440296678/nthu3cr9faf1zvgn9jkb.png", thumb_url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/c_scale,h_150,w_150/v1440296678/nthu3cr9faf1zvgn9jkb.png", imageable_id: 3, imageable_type: "Team"},
+  {url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/v1440296844/xyweb4kp7bbz8zxzvpa8.jpg", thumb_url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/c_scale,h_150,w_150/v1440296844/xyweb4kp7bbz8zxzvpa8.jpg", imageable_id: 4, imageable_type: "Team"},
+  {url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/v1440296268/naslsnz01rlqswwbsmsr.jpg", thumb_url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/c_scale,h_150,w_150/v1440296268/naslsnz01rlqswwbsmsr.jpg", imageable_id: 5, imageable_type: "Team"},
+  {url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/v1440296946/r0aqpdylbq37m1bpn9ds.jpg", thumb_url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/c_scale,h_150,w_150/v1440296946/r0aqpdylbq37m1bpn9ds.jpg", imageable_id: 6, imageable_type: "Team"},
+  {url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/v1440297049/xtasoavag0e7d1tfg2rq.jpg", thumb_url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/c_scale,h_150,w_150/v1440297049/xtasoavag0e7d1tfg2rq.jpg", imageable_id: 7, imageable_type: "Team"}
+])
 
-team2 = Team.create(name: "Crouching Tigers", owner_id: user2.id, description: "No growth without assistance. No action without reaction. No desire without restraint. Now give yourself up and find yourself again.")
-team3 = Team.create(name: "Hidden Dragons", owner_id: user3.id, description: "Fighters have rules, too. Friendship, trust, integrity. Always keep your promise. Without rules we wouldn't survive long.")
-team4 = Team.create(name: "Average Joe's", owner_id: user4.id, description: "Average Joe's is a local team named under the gym they are trying to save from bankruptcy. Led by its owner Pete LaFleur and coached by dodgeball legend 'Patches' O'Houlihan, they hope to win all dodgeball tournaments to keep their gym afloat.")
-team5 = Team.create(name: "Purple Cobras", owner_id: user5.id, description: "You ready for the, whoooo, hurricane?")
-team6 = Team.create(name: "The Patches", owner_id: user6.id, description: "Devastated by the loss of Patches in the movie Dodgeball, a group of fanatics have teamed up to pay their respects to Patches with one last tournament.")
-team7 = Team.create(name: "Legends", owner_id: user7.id, description: "The Man, The Myth, The Legend has graced the tournament floor. Do you dare stand in his way to glory?")
-team8 = Team.create(name: "Chuck Norris", owner_id: user8.id, description: "Fear of spiders is called arachnophobia, fear of tight spaces is called claustrophobia, fear of Chuck Norris is called logic.")
+Tournament.create!([
+  {author_id: 1, title: "1st Tournament Tutorial", description: "Welcome to the first tournament ever hosted on TournaGen! This is an example of a tournament starting from scratch. Take a look around!", max_teams: 32, results: "[[[[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1]],[[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1],[2,1]],[[2,1],[2,1],[2,1],[2,1]],[[2,1],[2,1]],[[2,1],[1,2]]]]", double_elim: false},
+  {author_id: 2, title: "If You can Dodge a Wrench", description: "Welcome to the official dogeball tournament for TournaGen!", max_teams: 8, results: "[]", double_elim: false},
+  {author_id: 1, title: "May Madness", description: "It's the 31st annual May Madness tournament! Live out your predictions for March Madness here. Disclaimer: None of these teams are actually the NCAA teams they're portraying.", max_teams: 8, results: "[]", double_elim: false}
+])
 
-img2 = Image.create(url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/v1440296455/dlm1nl0gjnktjtto3mmn.jpg", thumb_url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/c_scale,h_150,w_150/v1440296455/dlm1nl0gjnktjtto3mmn.jpg", imageable_id: team2.id, imageable_type: :Team)
-img3 = Image.create(url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/v1440296658/z1jhnctvhaayzz4hij7w.jpg", thumb_url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/c_scale,h_150,w_150/v1440296658/z1jhnctvhaayzz4hij7w.jpg", imageable_id: team3.id, imageable_type: :Team)
-img4 = Image.create(url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/v1440296678/nthu3cr9faf1zvgn9jkb.png", thumb_url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/c_scale,h_150,w_150/v1440296678/nthu3cr9faf1zvgn9jkb.png", imageable_id: team4.id, imageable_type: :Team)
-img5 = Image.create(url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/v1440296844/xyweb4kp7bbz8zxzvpa8.jpg", thumb_url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/c_scale,h_150,w_150/v1440296844/xyweb4kp7bbz8zxzvpa8.jpg", imageable_id: team5.id, imageable_type: :Team)
-img6 = Image.create(url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/v1440296268/naslsnz01rlqswwbsmsr.jpg", thumb_url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/c_scale,h_150,w_150/v1440296268/naslsnz01rlqswwbsmsr.jpg", imageable_id: team6.id, imageable_type: :Team)
-img7 = Image.create(url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/v1440296946/r0aqpdylbq37m1bpn9ds.jpg", thumb_url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/c_scale,h_150,w_150/v1440296946/r0aqpdylbq37m1bpn9ds.jpg", imageable_id: team7.id, imageable_type: :Team)
-img8 = Image.create(url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/v1440297049/xtasoavag0e7d1tfg2rq.jpg", thumb_url: "https://res.cloudinary.com/dlrvqt6fn/image/upload/c_scale,h_150,w_150/v1440297049/xtasoavag0e7d1tfg2rq.jpg", imageable_id: team8.id, imageable_type: :Team)
+Follow.create!([
+  {tournament_id: 1, follower_id: 2},
+  {tournament_id: 1, follower_id: 3},
+  {tournament_id: 1, follower_id: 4},
+  {tournament_id: 1, follower_id: 5},
+  {tournament_id: 1, follower_id: 6},
+  {tournament_id: 1, follower_id: 7},
+  {tournament_id: 1, follower_id: 8},
+  {tournament_id: 2, follower_id: 1},
+  {tournament_id: 2, follower_id: 2},
+  {tournament_id: 2, follower_id: 3},
+  {tournament_id: 2, follower_id: 4},
+  {tournament_id: 2, follower_id: 5},
+  {tournament_id: 3, follower_id: 1}
+])
 
+Registration.create!([
+  {tournament_id: 1, team_id: 1},
+  {tournament_id: 1, team_id: 2},
+  {tournament_id: 1, team_id: 3},
+  {tournament_id: 1, team_id: 4},
+  {tournament_id: 1, team_id: 5},
+  {tournament_id: 1, team_id: 6},
+  {tournament_id: 1, team_id: 7},
+  {tournament_id: 2, team_id: 1},
+  {tournament_id: 2, team_id: 2},
+  {tournament_id: 2, team_id: 3},
+  {tournament_id: 2, team_id: 4},
+  {tournament_id: 2, team_id: 5},
+  {tournament_id: 2, team_id: 6},
+  {tournament_id: 2, team_id: 7}
+])
 
-reg1 = Registration.create(tournament_id: tourn1.id, team_id: team2.id)
-reg2 = Registration.create(tournament_id: tourn1.id, team_id: team3.id)
-reg3 = Registration.create(tournament_id: tourn1.id, team_id: team4.id)
-reg4 = Registration.create(tournament_id: tourn1.id, team_id: team5.id)
-reg5 = Registration.create(tournament_id: tourn1.id, team_id: team6.id)
-reg6 = Registration.create(tournament_id: tourn1.id, team_id: team7.id)
-reg7 = Registration.create(tournament_id: tourn1.id, team_id: team8.id)
-reg8 = Registration.create(tournament_id: tourn2.id, team_id: team2.id)
-reg9 = Registration.create(tournament_id: tourn2.id, team_id: team3.id)
-reg10 = Registration.create(tournament_id: tourn2.id, team_id: team4.id)
-reg11 = Registration.create(tournament_id: tourn2.id, team_id: team5.id)
-reg12 = Registration.create(tournament_id: tourn2.id, team_id: team6.id)
-reg13 = Registration.create(tournament_id: tourn2.id, team_id: team7.id)
-reg14 = Registration.create(tournament_id: tourn2.id, team_id: team8.id)
-
-foll1 = Follow.create(tournament_id: tourn1.id, follower_id: user2.id)
-foll2 = Follow.create(tournament_id: tourn1.id, follower_id: user3.id)
-foll3 = Follow.create(tournament_id: tourn1.id, follower_id: user4.id)
-foll4 = Follow.create(tournament_id: tourn1.id, follower_id: user5.id)
-foll5 = Follow.create(tournament_id: tourn1.id, follower_id: user6.id)
-foll6 = Follow.create(tournament_id: tourn1.id, follower_id: user7.id)
-foll7 = Follow.create(tournament_id: tourn1.id, follower_id: user8.id)
-foll8 = Follow.create(tournament_id: tourn2.id, follower_id: user1.id)
-foll9 = Follow.create(tournament_id: tourn2.id, follower_id: user2.id)
-foll10 = Follow.create(tournament_id: tourn2.id, follower_id: user3.id)
-foll11 = Follow.create(tournament_id: tourn2.id, follower_id: user4.id)
-foll12 = Follow.create(tournament_id: tourn2.id, follower_id: user5.id)
-foll13 = Follow.create(tournament_id: tourn3.id, follower_id: user1.id)
-
-tm1 = TeamMembership.create(team_id: team2.id, user_id: user1.id)
+TeamMembership.create!([
+  {team_id: 1, user_id: 1}
+])
