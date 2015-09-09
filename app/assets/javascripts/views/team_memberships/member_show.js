e7f59c246c3f7820ba8b00498182db2b121cd269
darrengtan <darrengtan@gmail.com>
2015-09-08 21:18:29 -0700

    more comments

--- app/assets/javascripts/views/team_memberships/member_show.js
+++ app/assets/javascripts/views/team_memberships/member_show.js
@@ -1,12 +1,12 @@
 TournaGen.Views.MemberShow = Backbone.View.extend({
   template: JST["team_memberships/member_show"],
-
-  initialize: function () {
-    this.listenTo(this.model, "sync", this.render);
-  },
-
   tagName: "li",
   className: "list-group-item",
+
+  initialize: function () {
+    // update team member list on sync
+    this.listenTo(this.model, "sync", this.render);
+  },
 
   render: function () {
     this.$el.html(this.template({ teamMembership: this.model }));
