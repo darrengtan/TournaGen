5c38d988fe46ac6df4cc4264d9e1a328cfec2d14
darrengtan <darrengtan@gmail.com>
2015-09-09 13:47:43 -0700

    tournaments index comments

--- app/assets/javascripts/views/tournaments/tournaments_index.js
+++ app/assets/javascripts/views/tournaments/tournaments_index.js
@@ -15,6 +15,7 @@ TournaGen.Views.TournamentsIndex = Backbone.CompositeView.extend({
     this.addSubview("ul.tournaments-index", view);
   },
 
+  // show more tournaments upon scrolling to the bottom of page
   listenForScroll: function () {
     $(window).off("scroll");
     var throttledCallback = _.throttle(this.fetchMoreTournaments.bind(this), 500);
@@ -41,6 +42,7 @@ TournaGen.Views.TournamentsIndex = Backbone.CompositeView.extend({
 
   render: function () {
     this.$el.html(this.template({ tournaments: this.collection }));
+    // show loading spinner while fetching
     if (this.collection.fetching) {
       this.$(".tournaments-index").html(JST["loading_spinner"]());
       this.collection.fetching = false;
