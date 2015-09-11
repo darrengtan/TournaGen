25fb6e657c032515c955726dc60660ee3e136b49
darrengtan <darrengtan@gmail.com>
2015-09-11 13:08:55 -0700

    user rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.create(:user) }
+
+  it "generates session token upon creation" do
+    expect(user.session_token).to_not be_nil
+  end
+
+  it "resets token" do
+    old_session = user.session_token
+    expect(user.reset_token!).to_not eq(old_session)
+  end
 end
